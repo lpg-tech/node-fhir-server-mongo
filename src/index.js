c3d5565b9103cd596a1ce79dabba56233e092251
lpg-tech <laborprojektgruppe@gmail.oom>
2025-08-07 17:32:53 +0200

    verify connection is alive

--- src/index.js
+++ src/index.js
@@ -9,13 +9,32 @@ const { fhirServerConfig, mongoConfig } = require('./config');
 const { CLIENT, CLIENT_DB } = require('./constants');
 
 let main = async function () {
+
+  // Add a timeout to fail fast if the database is not available
+  const connectionOptions = Object.assign({}, mongoConfig.options, {
+    serverSelectionTimeoutMS: 5000, // Fail after 5 seconds if no server is available
+  });
+
   // Connect to mongo and pass any options here
+
   let [mongoErr, client] = await asyncHandler(
-    mongoClient(mongoConfig.connection, mongoConfig.options)
+    mongoClient(mongoConfig.connection, connectionOptions)
   );
+
+  // If the client is created, we still need to verify the connection is alive
+  if (!mongoErr && client) {
+    // The driver connects lazily and may not throw an error until the first operation.
+    // We can ping the admin database to ensure a connection is established.
+    const [pingErr] = await asyncHandler(client.db('admin').command({ ping: 1 }));
+    // If the ping fails, we will treat that as the connection error.
+    if (pingErr) {
+      mongoErr = pingErr;
+    }
+  }
 
   if (mongoErr) {
     console.error(mongoErr.message);
+    console.error('❌ Could not connect to the database. 🔍 Is the service running?');
     console.error(mongoConfig.connection);
     process.exit(1);
   }
@@ -25,8 +44,10 @@ let main = async function () {
   globals.set(CLIENT_DB, client.db(mongoConfig.db_name));
 
   // Start our FHIR server
-  let server = FHIRServer.initialize(fhirServerConfig);
-  server.listen(fhirServerConfig.server.port, () => logger.verbose('Server is up and running!'));
+  const server = FHIRServer.initialize(fhirServerConfig);
+
+  logger.verbose('✅ Connected to MongoDB');
+  server.listen(fhirServerConfig.server.port, () => logger.verbose('✅ Server is up and running!'));
 };
 
 main();
